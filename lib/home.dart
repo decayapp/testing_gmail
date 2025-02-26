244f4ab6805c9159c86b60c15d9bb05dc91fe257
lazy programmer <testingapp10101@gmail.com>
2025-02-26 16:03:24 +0000

    code  improve

--- lib/home.dart
+++ lib/home.dart
@@ -10,24 +10,37 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  var alert = "";
+  final txtemail = TextEditingController();
+  final txtsubject = TextEditingController();
+  final txtmsg = TextEditingController();
+  var lbl_text = "";
+
   Future<void> sendingemail() async {
     try {
       var message = Message();
       var userEmail = 'testingapp10101@gmail.com';
-      message.subject = 'Sending subject from flutter';
-      message.text = 'get my email....';
+      var password = ''; // Replace with your app password
 
+      message.subject = txtsubject.text;
+      message.text = txtmsg.text;
       message.from = userEmail;
-      message.recipients.add(userEmail);
+      message.recipients.add(txtemail.text);
 
-      var smtpServer = gmail(userEmail, 'app_password');   //use app password from gmail
-      
-      
-      send(message, smtpServer);
-      print('sent');
+      var smtpServer = gmail(userEmail, password);
+
+      await send(message, smtpServer);
+      setState(() {
+        lbl_text = 'Email Sent Successfully!';
+      });
     } catch (e) {
-      print('${e.toString()}');
+      print(e.toString());
+      setState(() {
+        lbl_text = 'Failed to send email: ${e.toString()}';
+      });
+    } finally {
+      txtemail.clear();
+      txtsubject.clear();
+      txtmsg.clear();
     }
   }
 
@@ -36,11 +49,36 @@ class _HomeState extends State<Home> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Mail  from  flutter'),
+          backgroundColor: Colors.pink,
+          title: const Text('Mail  from  flutter'),
           centerTitle: true,
         ),
         body: Container(
-          child: OutlinedButton(onPressed: sendingemail, child: Text('send')),
+          margin: const EdgeInsets.fromLTRB(20, 10, 20, 0),
+          child: Column(
+            children: [
+              TextField(
+                controller: txtemail,
+                decoration: const InputDecoration(hintText: 'Enter email...'),
+              ),
+              TextField(
+                controller: txtsubject,
+                decoration: const InputDecoration(hintText: 'Enter Subject...'),
+              ),
+              TextField(
+                controller: txtmsg,
+                decoration: const InputDecoration(hintText: 'Enter Message...'),
+              ),
+              OutlinedButton(
+                onPressed: sendingemail,
+                child: const Text('Send'),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: Text(lbl_text),
+              )
+            ],
+          ),
         ),
       ),
     );
